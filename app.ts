24e577bea114681d785dd562139d5906df54cc4e
BraveVlad <giladvladpinker@gmail.com>
2023-11-25 12:50:16 +0200

    inital FoldableMenu implementation

--- app.ts
+++ app.ts
@@ -23,13 +23,33 @@ function createChiller(chiller: Omit<Chiller, "liquidCurrent" | "lastChecked">) 
     }
 
     chillers.push(newChiller);
+}
 
+
+type MenuObject = {
+    id: string,
+    title: string
+}
+
+const PERFIX_FOLDABLE_MENU = ".foldable-menu"
+const menu = document.querySelector(PERFIX_FOLDABLE_MENU) as HTMLDivElement;
+
+const title = menu.querySelector(PERFIX_FOLDABLE_MENU + "__title") as HTMLHeadingElement;
+const amount = menu.querySelector(PERFIX_FOLDABLE_MENU + "__amount") as HTMLParagraphElement;
+const amountTitle = menu.querySelector(PERFIX_FOLDABLE_MENU + "__amount-title") as HTMLSpanElement;
+const createButton = menu.querySelector(PERFIX_FOLDABLE_MENU + "__create") as HTMLButtonElement;
+
+const list = menu.querySelector(PERFIX_FOLDABLE_MENU + "__list") as HTMLUListElement;
+//, create: Function, list: MenuObject[]
+function setTitle(menuTitle: string) {
+    title.innerText = menuTitle;
 }
 
 function main() {
 
     console.log("Welcome to Chiller. app!")
 
+    setTitle("Hello");
 }
 
 
--- dist/app.js
+++ dist/app.js
@@ -7,7 +7,19 @@ function createChiller(chiller) {
     };
     chillers.push(newChiller);
 }
+const PERFIX_FOLDABLE_MENU = ".foldable-menu";
+const menu = document.querySelector(PERFIX_FOLDABLE_MENU);
+const title = menu.querySelector(PERFIX_FOLDABLE_MENU + "__title");
+const amount = menu.querySelector(PERFIX_FOLDABLE_MENU + "__amount");
+const amountTitle = menu.querySelector(PERFIX_FOLDABLE_MENU + "__amount-title");
+const createButton = menu.querySelector(PERFIX_FOLDABLE_MENU + "__create");
+const list = menu.querySelector(PERFIX_FOLDABLE_MENU + "__list");
+//, create: Function, list: MenuObject[]
+function setTitle(menuTitle) {
+    title.innerText = menuTitle;
+}
 function main() {
     console.log("Welcome to Chiller. app!");
+    setTitle("Hello");
 }
 main();
