45b9ad8730c497f9096e8c41d3a6411fd48a4a12
BraveVlad <giladvladpinker@gmail.com>
2023-11-25 13:21:46 +0200

    implemented foldablemenu view logic

--- app.ts
+++ app.ts
@@ -32,24 +32,93 @@ type MenuObject = {
 }
 
 const PERFIX_FOLDABLE_MENU = ".foldable-menu"
+const PERFIX_HIDE_MENU = "foldable-menu--hidden"
+
 const menu = document.querySelector(PERFIX_FOLDABLE_MENU) as HTMLDivElement;
 
 const title = menu.querySelector(PERFIX_FOLDABLE_MENU + "__title") as HTMLHeadingElement;
-const amount = menu.querySelector(PERFIX_FOLDABLE_MENU + "__amount") as HTMLParagraphElement;
+const amountNumber = menu.querySelector(PERFIX_FOLDABLE_MENU + "__amount") as HTMLParagraphElement;
 const amountTitle = menu.querySelector(PERFIX_FOLDABLE_MENU + "__amount-title") as HTMLSpanElement;
 const createButton = menu.querySelector(PERFIX_FOLDABLE_MENU + "__create") as HTMLButtonElement;
 
 const list = menu.querySelector(PERFIX_FOLDABLE_MENU + "__list") as HTMLUListElement;
-//, create: Function, list: MenuObject[]
+
+// view
 function setTitle(menuTitle: string) {
     title.innerText = menuTitle;
 }
+
+// view
+function setAmount(amount: number, countedObjectsName: string) {
+    amountNumber.innerHTML = amount.toString() + " " + countedObjectsName;
+}
+
+// controller
+type CreateCallback = (event: MouseEvent) => void;
+function setCreateCallback(createCallback: CreateCallback) {
+    createButton.addEventListener("click", createCallback);
+}
+
+// view
+function renderList(objects: MenuObject[]) {
+
+    list.replaceChildren();
+    list.innerHTML = `
+    ${objects.map(generateItemElement).join('\n')
+        }
+    `
+}
+
+// view 
+function generateItemElement(object: MenuObject) {
+    return `
+    <li class="foldable-menu__item">
+        <button class="foldable-menu__object" data-selected-item-id="${object.id}" >${object.title}</button>
+    </li>
+    `
+}
+
+function show() {
+    menu.classList.remove(PERFIX_HIDE_MENU);
+}
+
+function hide() {
+    menu.classList.add(PERFIX_HIDE_MENU);
+}
+
 
 function main() {
 
     console.log("Welcome to Chiller. app!")
 
-    setTitle("Hello");
+    setTitle("hello");
+    setAmount(123, "Chillers");
+    setCreateCallback((event) => {
+        console.log(event)
+    });
+
+    const test: MenuObject[] = [
+        {
+            id: "1",
+            title: "first item"
+        },
+        {
+            id: "2",
+            title: "first item"
+        },
+        {
+            id: "3",
+            title: "first item"
+        },
+        {
+            id: "4",
+            title: "first item"
+        }
+    ]
+
+    renderList(test);
+    hide();
+    show();
 }
 
 
--- dist/app.js
+++ dist/app.js
@@ -8,18 +8,72 @@ function createChiller(chiller) {
     chillers.push(newChiller);
 }
 const PERFIX_FOLDABLE_MENU = ".foldable-menu";
+const PERFIX_HIDE_MENU = "foldable-menu--hidden";
 const menu = document.querySelector(PERFIX_FOLDABLE_MENU);
 const title = menu.querySelector(PERFIX_FOLDABLE_MENU + "__title");
-const amount = menu.querySelector(PERFIX_FOLDABLE_MENU + "__amount");
+const amountNumber = menu.querySelector(PERFIX_FOLDABLE_MENU + "__amount");
 const amountTitle = menu.querySelector(PERFIX_FOLDABLE_MENU + "__amount-title");
 const createButton = menu.querySelector(PERFIX_FOLDABLE_MENU + "__create");
 const list = menu.querySelector(PERFIX_FOLDABLE_MENU + "__list");
-//, create: Function, list: MenuObject[]
+// view
 function setTitle(menuTitle) {
     title.innerText = menuTitle;
 }
+// view
+function setAmount(amount, countedObjectsName) {
+    amountNumber.innerHTML = amount.toString() + " " + countedObjectsName;
+}
+function setCreateCallback(createCallback) {
+    createButton.addEventListener("click", createCallback);
+}
+// view
+function renderList(objects) {
+    list.replaceChildren();
+    list.innerHTML = `
+    ${objects.map(generateItemElement).join('\n')}
+    `;
+}
+// view 
+function generateItemElement(object) {
+    return `
+    <li class="foldable-menu__item">
+        <button class="foldable-menu__object" data-selected-item-id="${object.id}" >${object.title}</button>
+    </li>
+    `;
+}
+function show() {
+    menu.classList.remove(PERFIX_HIDE_MENU);
+}
+function hide() {
+    menu.classList.add(PERFIX_HIDE_MENU);
+}
 function main() {
     console.log("Welcome to Chiller. app!");
-    setTitle("Hello");
+    setTitle("hello");
+    setAmount(123, "Chillers");
+    setCreateCallback((event) => {
+        console.log(event);
+    });
+    const test = [
+        {
+            id: "1",
+            title: "first item"
+        },
+        {
+            id: "2",
+            title: "first item"
+        },
+        {
+            id: "3",
+            title: "first item"
+        },
+        {
+            id: "4",
+            title: "first item"
+        }
+    ];
+    renderList(test);
+    hide();
+    show();
 }
 main();
